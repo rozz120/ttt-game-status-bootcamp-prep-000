092d7a1345f85c42c643053bb2143707a89dbe1f
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 09:44:37 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,24 +26,7 @@ def won?(board)
     win_index_2 = win_Number[1]
     win_index_3 = win_Number[2]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    
-    if position_1 == "X" && position_2 == "X"  && position_3 == "X"
-      
-        return win_combinations
-        
-    elsif position_1 == "O" &&  position_2 == "O" && position_3 == "O"
-    
-        return win_combinations
-    else
-      
-      return false
-    end
-  end
-  end
-end
+ 
     
     
     
