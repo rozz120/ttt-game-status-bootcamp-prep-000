0256f01e59d1a665839041c9bb3dc1d915fca97f
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 10:46:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -71,15 +71,13 @@ end
 
 
 def over?(board)
-  
+  puts board
   if draw?(board) == true
     return true
   
   elsif won?(board) == true && full?(board) == false
     return true
-    
  
-    
   end
 end
     
