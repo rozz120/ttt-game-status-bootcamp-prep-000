0294f5ddaacc0d97792f02c90830719aa1463e16
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 08:48:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def won?
+
   
   WIN_COMBINATIONS [
   [0,1,2]
@@ -14,7 +14,6 @@ def won?
   [2,5,8]
   [0,4,8]
   [2,4,6]
-  
-  ]
+    ]
 
  end
