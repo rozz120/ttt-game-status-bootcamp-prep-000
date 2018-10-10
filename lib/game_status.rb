0b1e9d8700f1f4ea7b6732a8a91a4e9702421541
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 10:11:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,7 +45,7 @@ def full?(board)
   board.each do |is_board_full|
     
   if is_board_full == "X" || is_board_full == "O"
-      return true
+      puts is_board_full
       
   else
       return false
