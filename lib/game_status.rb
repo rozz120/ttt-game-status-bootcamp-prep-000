9d456c63cc4ed85784fe87dca673d0952fbe6bf0
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 09:43:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,7 +30,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
-    if (position_1 == "X" && position_2 == "X"  && position_3 == "X")
+    if position_1 == "X" && position_2 == "X"  && position_3 == "X"
       puts win_combinations
         return win_combinations
         
