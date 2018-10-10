35ee3ca4ed8fe8540193aa8a2de201e0f08fffea
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 08:54:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,23 @@ end
 
 
   
-  WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-    ]
+WIN_COMBINATIONS = [
+  [0,1,2],  //top row
+  [3,4,5],  // middle row
+  [6,7,8],  // bottom row
+  [0,3,6],  // first collumn
+  [1,4,7],  // second collumn
+  [2,5,8],  // third collumn
+  [0,4,8],  // top left, to bottom right
+  [2,4,6]   // top right, to bottom left
+]
+
+
+def won?(board)
+  
+end
+    
+    
+    
 
  
