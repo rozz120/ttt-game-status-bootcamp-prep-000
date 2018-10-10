cef1a6d16d0a86fee425b948b4090f2e6902b640
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 09:26:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,10 +19,11 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combinations|
+    win_combinations.each do |win_position|
     
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    win_index_1 = win_position[0]
+    win_index_2 = win_position[1]
+    win_index_3 = win_position[2]
     
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
