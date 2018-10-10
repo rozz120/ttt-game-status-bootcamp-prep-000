47b3a43a8344da54bf746bc3d27d059305eded85
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-10 09:16:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,6 +26,9 @@ def won?(board)
     
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_1 == "X"
     
     
     
